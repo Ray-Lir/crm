1bae6525e9930226ea5f6afd46c2f956f77dde34
Ray Li <ray.lir@outlook.com>
2020-05-26 23:27:42 +0800

    新增登录拦截功能

--- src/main/java/com/ray/crm/interceptor/LoginInterceptor.java
+++ src/main/java/com/ray/crm/interceptor/LoginInterceptor.java
@@ -1,5 +1,7 @@
 package com.ray.crm.interceptor;
 
+import com.ray.crm.exception.InterceptorException;
+import com.ray.crm.settings.domain.User;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -7,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * @Description:
+ * @Description: 登录拦截
  * @Author: Ray Li
  * @E-mail: ray.lir@outlook.com
  * @Date: 2020/5/26 20:02
@@ -15,6 +17,22 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        /*
+            通过session中是否有user对象来判断登录状态
+            如果登录，则返回true（将请求放行）
+            如果没有登录过，则回到登录页
+
+         */
+
+        User user = (User) request.getSession().getAttribute("user");
+
+        if (user == null) {
+
+            //说明没有登录过
+            throw new InterceptorException();
+
+        }
         return true;
     }
 
